34dfa1cc89a95f8fc5f2849618cc6c809171e963
Withawat Phiraban <withawat1201@gmail.com>
2026-07-17 15:56:41 +0700

    Do! task-1 postgersql and bonus

--- 02_querying_assessment/postgresql/bonus/query_task1_bonus.mongodb.js
+++ 02_querying_assessment/postgresql/bonus/query_task1_bonus.mongodb.js
@@ -10,6 +10,9 @@
 
 // ---------------------------------------------------------------
 // Your thinking process (required)
+use("chrome-burger-db");
+
+db.menu_items.find({ category: "Side" }, { name: 1, price: 1, _id: 0 });
 // ---------------------------------------------------------------
 // Before writing your query, explain in your own words how you
 // interpreted the task, what data you need, which collection(s)
@@ -17,4 +20,8 @@
 // Write in English or Thai. Do not skip this step.
 //
 // Your thinking:
+// กำหนดให้ดึงข้อมูลมาจาก database ชื่อ chrome-burger-db โดยใช้ use
+// ใช้คำสั่ง db เพื่อให้อิงข้อมูลมาจา่ก collection ที่ชื่อว่า menu_items
+// ใช้คำสั่ง find เพื่อหาข้อมูลที่ต้องการโดยในวงเล็บจะเป็นเงื่อนไขพารามิเตอร์แรกคือกำหนดให้หาแค่ document ที่มี field ชื่อว่า category มือชื่อว่า Side
+// พารามิเตอร์ที่2คือให้ตั้งเงื่อไขให้แสดงแค่ name กับ price และให้ซ่อน id ไว้
 //
--- 02_querying_assessment/postgresql/query_task1.sql
+++ 02_querying_assessment/postgresql/query_task1.sql
@@ -4,17 +4,22 @@
 -- so the designer can produce an accurate board without having to check each item manually.
 --
 -- Hint: Write a query to find the name and price of all menu items that are in the 'Side' category.
-
--- Bonus: The dataset is identical in the MongoDB database, meaning the same business insight can be retrieved.
+ -- Bonus: The dataset is identical in the MongoDB database, meaning the same business insight can be retrieved.
 -- Write the equivalent query for MongoDB. See query_task1_bonus.mongodb.js
-
--- ---------------------------------------------------------------
+ -- ---------------------------------------------------------------
 -- Your thinking process (required)
--- ---------------------------------------------------------------
+
+SELECT name,
+       price
+FROM menuitems
+WHERE category = 'Side' -- ---------------------------------------------------------------
 -- Before writing your query, explain in your own words how you
 -- interpreted the task, what data you need, which table(s) are
 -- involved, and what SQL concepts you plan to use.
 -- Write in English or Thai. Do not skip this step.
 --
 -- Your thinking:
+-- ใช้คำสั่ง SELECT เพื่อเรียกดึงข้อมูลมา โดยกำหนดให้ดึงแค่ name และ price
+-- ใช้ FROM เพิ่งชี้ว่าเอามาจากตารางไหน
+-- กำหนดเงื่อนไขโดยการใช้ WHERE และตั้งเงื่อนไขให้ category มีค่าเท่ากับ Side
 --
